3326ce6535c695f3582e62d74926e7acab4a41ed
JoRaso <jonathan.raso@le1817.com>
2022-12-08 17:55:42 +0100

    Commenting Login part because not working (need to fix this later)

--- app.py
+++ app.py
@@ -34,47 +34,52 @@ st.title("RC28 Prédiction")
 url = 'https://filrouge-backend.onrender.com/predict'
 headers = {'content-type': 'application/json'}
 
+##################### CHECK WHY SESSIONS DOESNT WORK WHEN DEPLOYED
+# usermail = "testmail@le1817.com"
+# password = "granulo"
 
-# if uploaded_file is not None:
-#     payload = load_file(uploaded_file)
-
-#     if st.button("Prédiction"):
-#         send_sample(payload, url, headers)
-
-usermail = os.getenv('USERMAIL')
-password = os.getenv('PWD')
-
-# Initialization
-if "logged" not in st.session_state:
-    st.session_state["logged"] = False
+# # Initialization
+# if "logged" not in st.session_state:
+#     st.session_state["logged"] = False
 
 
-# login form
-if st.session_state["logged"] == False:
+# # login form
+# if st.session_state["logged"] == False:
 
-    placeholder = st.empty()
+#     placeholder = st.empty()
 
-    with placeholder.form("login"):
-        st.markdown("#### Bonjour, veuillez renseigner vos identifiants")
-        user_email = st.text_input(label="Email", placeholder="votremail@exemple.com")
-        user_password = st.text_input(label="Mot de passe", placeholder="Enter votre mot de passe", type="password")
-        login_button = st.form_submit_button("Login")
+#     with placeholder.form("login"):
+#         st.markdown("#### Bonjour, veuillez renseigner vos identifiants")
+#         user_email = st.text_input(label="Email", placeholder="votremail@exemple.com")
+#         user_password = st.text_input(label="Mot de passe", placeholder="Enter votre mot de passe", type="password")
+#         login_button = st.form_submit_button("Login")
 
-        if ((login_button) and (user_email == usermail) and (user_password == password)):
-            st.session_state["logged"] = True
-            placeholder.empty()
-        elif ((login_button) and ((user_email != usermail) or (user_password != password))):
-            st.error('Identifiants incorrects, veuillez réessayer', icon="⚠️")
-else:
-    if st.button("Déconnexion"):
-        del st.session_state["logged"]
-            
+#         if ((login_button) and (user_email == usermail) and (user_password == password)):
+#             st.session_state["logged"] = True
+#             placeholder.empty()
+#         elif ((login_button) and ((user_email != usermail) or (user_password != password))):
+#             st.error('Identifiants incorrects, veuillez réessayer', icon="⚠️")
+# else:
+#     if st.button("Déconnexion"):
+#         del st.session_state["logged"]
+# # loading file and sending sample for prediction
+# if st.session_state["logged"]:
+#     st.text("Bienvenue, vous allez pouvoir connaitre la résistance de votre béton en un clic !")
+#     uploaded_file = st.file_uploader("Téléverser un fichier csv")
+#     if uploaded_file is not None:
+#         payload = load_file(uploaded_file)
+
+#         if st.button("Prédiction"):
+#             send_sample(payload, url, headers)
+##################### CHECK WHY SESSIONS DOESNT WORK WHEN DEPLOYED
+
+
 # loading file and sending sample for prediction
-if st.session_state["logged"]:
-    st.text("Bienvenue, vous allez pouvoir connaitre la résistance de votre béton en un clic !")
-    uploaded_file = st.file_uploader("Téléverser un fichier csv")
-    if uploaded_file is not None:
-        payload = load_file(uploaded_file)
 
-        if st.button("Prédiction"):
-            send_sample(payload, url, headers)
+st.text("Bienvenue, vous allez pouvoir connaitre la résistance de votre béton en un clic !")
+uploaded_file = st.file_uploader("Téléverser un fichier csv")
+if uploaded_file is not None:
+    payload = load_file(uploaded_file)
+
+    if st.button("Prédiction"):
+        send_sample(payload, url, headers)
