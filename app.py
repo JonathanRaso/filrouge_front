a54352c85980b277e4da3ad130caa03ed387f76f
JoRaso <jonathan.raso@le1817.com>
2022-12-09 18:28:45 +0100

    Testing render env var print

--- app.py
+++ app.py
@@ -10,38 +10,119 @@ def load_file(uploaded_file):
     dataframe = pd.read_csv(uploaded_file, index_col=[0])
     st.write("L'échantillon a bien été chargé !")
     first_row = dataframe.head(1)
-
     # create payload
     payload = {}
     for i, col in enumerate(first_row):
         payload[first_row.columns[i]] = first_row[col][0]
-
     # return values for prediction      
     return payload
 
+def send_login(email, password):
+    payload = {'email': email, 'password': password}
+    resp = requests.post(url=f"{url}/login", json=payload, headers=headers)
+    return resp
+
 def send_sample(payload, server_url, headers):
-    resp = requests.post(url=url, json=payload, headers=headers)
+    resp = requests.post(url=f"{url}/predict", json=payload, headers=headers)
     prediction = resp.text[:5]
     st.write(f"La prédiction de la résistance à 28 jours pour cet échantillon est de {prediction} MPa")
 
-### Application ###
-st.title("RC28 Prédiction")
+def change_is_logged_session():
+    st.session_state["is_logged"] = not st.session_state["is_logged"]
 
-### file upload
-# uploaded_file = st.file_uploader("Téléverser un fichier csv")
 
-# request params
-url = 'https://filrouge-backend.onrender.com/predict'
-headers = {'content-type': 'application/json'}
+st.title("Test")
+st.write(os.environ('PWD'))
+
+# # session state
+# if "is_logged" not in st.session_state:
+#     st.session_state["is_logged"] = False
+
+# ### Application ###
+# st.title("RC28 Prédiction")
+
+# # request params
+# url = 'http://127.0.0.1:8000'
+# # url = 'https://filrouge-backend.onrender.com'
+# headers = {'content-type': 'application/json'}
+
+
+# usermail = "test"
+# password = "root"
+
+
+# # login form
+# if st.session_state["is_logged"] == False:
+
+#     placeholder = st.empty()
+
+#     with placeholder.form("login"):
+#         st.markdown("#### Bonjour, veuillez renseigner vos identifiants")
+#         user_email = st.text_input(label="Email", placeholder="votremail@exemple.com")
+#         user_password = st.text_input(label="Mot de passe", placeholder="Enter votre mot de passe", type="password")
+#         login_button = st.form_submit_button("Login")
+#         if login_button:
+#             access_granted = send_login(user_email, user_password)
+#             if access_granted != "Connexion autorisée":
+#                 print(f"retour api = pas {access_granted}")
+#                 st.session_state["is_logged"]
+#                 change_is_logged_session()
+#                 st.session_state["is_logged"]
+#                 placeholder.empty()
+#             else:
+#                 st.error('Identifiants incorrects, veuillez réessayer', icon="⚠️")
+
+#             # if access_granted == "Connexion autorisée":
+#             #     change_is_logged_session()
+#             #     placeholder.empty()
+#             # else:
+#             #     st.error('Identifiants incorrects, veuillez réessayer', icon="⚠️")
+
+#         # if ((login_button) and (user_email == usermail) and (user_password == password)):
+#         #     change_is_logged_session()
+#         #     placeholder.empty()
+#         # elif ((login_button) and ((user_email != usermail) or (user_password != password))):
+#         #     st.error('Identifiants incorrects, veuillez réessayer', icon="⚠️")
+
+
+# if st.session_state["is_logged"] == True:
+#     st.write(st.session_state["is_logged"])
+#     # st.write(user_email, user_password)
+#     # print(user_email, user_password)
+#     st.button("Déconnexion", on_click=change_is_logged_session)
+    
+#     # loading file and sending sample for prediction
+#     st.text("Bienvenue, vous allez pouvoir connaitre la résistance de votre béton en un clic !")
+#     uploaded_file = st.file_uploader("Téléverser un fichier csv")
+#     if uploaded_file is not None:
+#         payload = load_file(uploaded_file)
+#         if st.button("Prédiction"):
+#             send_sample(payload, url, headers)
+
+
+
+
+
+
+# if st.session_state["logged"]:
+#     st.text("Bienvenue, vous allez pouvoir connaitre la résistance de votre béton en un clic !")
+#     uploaded_file = st.file_uploader("Téléverser un fichier csv")
+#     if uploaded_file is not None:
+#         payload = load_file(uploaded_file)
+
+#         if st.button("Prédiction"):
+#             send_sample(payload, url, headers)
+
+
+
+
+
+
+
 
 ##################### CHECK WHY SESSIONS DOESNT WORK WHEN DEPLOYED
 # usermail = "testmail@le1817.com"
 # password = "granulo"
-
-# # Initialization
-# if "logged" not in st.session_state:
-#     st.session_state["logged"] = False
-
 
 # # login form
 # if st.session_state["logged"] == False:
@@ -76,10 +157,10 @@ headers = {'content-type': 'application/json'}
 
 # loading file and sending sample for prediction
 
-st.text("Bienvenue, vous allez pouvoir connaitre la résistance de votre béton en un clic !")
-uploaded_file = st.file_uploader("Téléverser un fichier csv")
-if uploaded_file is not None:
-    payload = load_file(uploaded_file)
+# st.text("Bienvenue, vous allez pouvoir connaitre la résistance de votre béton en un clic !")
+# uploaded_file = st.file_uploader("Téléverser un fichier csv")
+# if uploaded_file is not None:
+#     payload = load_file(uploaded_file)
 
-    if st.button("Prédiction"):
-        send_sample(payload, url, headers)
+#     if st.button("Prédiction"):
+#         send_sample(payload, url, headers)
